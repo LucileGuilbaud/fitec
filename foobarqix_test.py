8459e4c9d4874d114e612d144e704b6aca36b276
LucileGuilbaud <33248739+LucileGuilbaud@users.noreply.github.com>
2017-10-31 14:22:05 +0100

    divisibilité multiple

--- foobarqix_test.py
+++ foobarqix_test.py
@@ -6,22 +6,30 @@ def is_divisible(number, divisor):
 
 
 def foobarqix(number):
+    result = ""
     if is_divisible(number, 3):
-        return "Foo"
+        result += "Foo"
     if is_divisible(number, 5):
-        return "Bar"
+        result += "Bar"
     if is_divisible(number, 7):
-        return "Qix"
+        result += "Qix"
+    return result
+
 
 
 class FooBarQixTestCase(unittest.TestCase):
     def test_return_Foo_when_number_is_divisible_by_3(self):
         self.assertEqual(foobarqix(3), "Foo")
+        self.assertEqual(foobarqix(6), "Foo")
+        self.assertEqual(foobarqix(9), "Foo")
 
     def test_return_Bar_when_number_is_divisible_by_5(self):
         self.assertEqual(foobarqix(5), "Bar")
 
     def test_return_Qix_when_number_is_divisible_by_7(self):
         self.assertEqual(foobarqix(7), "Qix")
+
+    def test_return_FooBar_when_number_is_divisible_by_5_and_3(self):
+        self.assertEqual(foobarqix(15), "FooBar")
 
 unittest.main()
